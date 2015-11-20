1e7364792173c68dc1a585ec5c59ff6d43ec0945
Brett <msea@gmx.com>
2015-11-20 15:47:42 +1300

    is_file > file_exists. removed function_exists check.

--- img_resize.php
+++ img_resize.php
@@ -8,39 +8,37 @@
  *	Function for resizing images on the fly in WordPress with resized image caching.
  */
 
-if(!function_exists('img_resize')) {
-	function img_resize($imageURL, $width, $height, $crop = false) {
-		$imageBase = str_replace(basename($imageURL), '', $imageURL);
-		$imageURLParts = parse_url($imageURL);
-		$imagePath = $_SERVER['DOCUMENT_ROOT'] . $imageURLParts['path'];
+function img_resize($imageURL, $width, $height, $crop = false) {
+	$imageBase = str_replace(basename($imageURL), '', $imageURL);
+	$imageURLParts = parse_url($imageURL);
+	$imagePath = $_SERVER['DOCUMENT_ROOT'] . $imageURLParts['path'];
 
-		if (!file_exists($imagePath))
-			return false;
+	if (!is_file($imagePath))
+		return false;
 
-		$originalSize = getimagesize($imagePath);
+	$originalSize = getimagesize($imagePath);
 
-		if ($originalSize[0] <= $width)
-			return $imageURL;
+	if ($originalSize[0] <= $width)
+		return $imageURL;
 
-		if (!$height)
-			$height = round($originalSize[1] / $originalSize[0] * $width);
+	if (!$height)
+		$height = round($originalSize[1] / $originalSize[0] * $width);
 
-		$pathInfo = pathinfo($imagePath);
-		$resizedImageFileName = $pathInfo['filename'] . '-' . $width . 'x' . $height . '.' . $pathInfo['extension'];
-		$resizedImageURL = $imageBase . $resizedImageFileName;
-		$resizedImagePath = $pathInfo['dirname'] . '/' . $resizedImageFileName;
+	$pathInfo = pathinfo($imagePath);
+	$resizedImageFileName = $pathInfo['filename'] . '-' . $width . 'x' . $height . '.' . $pathInfo['extension'];
+	$resizedImageURL = $imageBase . $resizedImageFileName;
+	$resizedImagePath = $pathInfo['dirname'] . '/' . $resizedImageFileName;
 
-		if (file_exists($resizedImagePath))
-			return $resizedImageURL;
-
-		$editor = wp_get_image_editor($imagePath);
-
-		if (is_wp_error($editor))
-			return false;
-
-		$editor->resize($width, $height, $crop ? array('center', 'center') : false);
-		$editor->save($resizedImagePath);
-
+	if (is_file($resizedImagePath))
 		return $resizedImageURL;
-	}
+
+	$editor = wp_get_image_editor($imagePath);
+
+	if (is_wp_error($editor))
+		return false;
+
+	$editor->resize($width, $height, $crop ? array('center', 'center') : false);
+	$editor->save($resizedImagePath);
+
+	return $resizedImageURL;
 }
